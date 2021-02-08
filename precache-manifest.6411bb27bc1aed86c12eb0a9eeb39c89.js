1f59c75c2b37a90583a07e206eea3e42211f0e53
Nathan <nate1898@gmail.com>
2021-02-07 22:31:31 -0500

    Updates

---
+++ precache-manifest.6411bb27bc1aed86c12eb0a9eeb39c89.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42eec38c3dd0b09115b71a6b83d0dc11",
+    "url": "/Workout-Tracker-fe-pt/index.html"
+  },
+  {
+    "revision": "2f022a1019d7513aeaf5",
+    "url": "/Workout-Tracker-fe-pt/static/css/2.b218db7a.chunk.css"
+  },
+  {
+    "revision": "a512b47e4dc7f2757b53",
+    "url": "/Workout-Tracker-fe-pt/static/css/main.cf17cdbc.chunk.css"
+  },
+  {
+    "revision": "2f022a1019d7513aeaf5",
+    "url": "/Workout-Tracker-fe-pt/static/js/2.eba2d450.chunk.js"
+  },
+  {
+    "revision": "69294cf6f52e66b0c47708e3cfac4fe6",
+    "url": "/Workout-Tracker-fe-pt/static/js/2.eba2d450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a512b47e4dc7f2757b53",
+    "url": "/Workout-Tracker-fe-pt/static/js/main.d5952872.chunk.js"
+  },
+  {
+    "revision": "30f8a8c68bd090feca12",
+    "url": "/Workout-Tracker-fe-pt/static/js/runtime-main.1728c169.js"
+  },
+  {
+    "revision": "27f7092d9a9ad6a01b2a1c28c05c0f6b",
+    "url": "/Workout-Tracker-fe-pt/static/media/activity.27f7092d.svg"
+  },
+  {
+    "revision": "fd018a77d1c060c555475a7e72c4579a",
+    "url": "/Workout-Tracker-fe-pt/static/media/checkmark.fd018a77.svg"
+  },
+  {
+    "revision": "f432131419c8a851fa436aed537ae766",
+    "url": "/Workout-Tracker-fe-pt/static/media/circle.f4321314.svg"
+  },
+  {
+    "revision": "392e010ab5db0424c8e364002bad5682",
+    "url": "/Workout-Tracker-fe-pt/static/media/circlesmall.392e010a.svg"
+  },
+  {
+    "revision": "d573f1e0953208f98a3b348ae85e9a3f",
+    "url": "/Workout-Tracker-fe-pt/static/media/cloudsync.d573f1e0.svg"
+  },
+  {
+    "revision": "4b02cf07c80b58865cab87b15eed1b29",
+    "url": "/Workout-Tracker-fe-pt/static/media/defaultavatar.4b02cf07.svg"
+  },
+  {
+    "revision": "e2d505d4bb2b9b338e646c72eb34e001",
+    "url": "/Workout-Tracker-fe-pt/static/media/diet1.e2d505d4.jpeg"
+  },
+  {
+    "revision": "dc8539400b47815fdb0462dec5a32b7e",
+    "url": "/Workout-Tracker-fe-pt/static/media/diet10.dc853940.jpeg"
+  },
+  {
+    "revision": "3d6cfe544cc88a53be6f0eaeced4c569",
+    "url": "/Workout-Tracker-fe-pt/static/media/diet11.3d6cfe54.jpeg"
+  },
+  {
+    "revision": "74aceb76c1f6dc5b52dbe123f36db843",
+    "url": "/Workout-Tracker-fe-pt/static/media/diet12.74aceb76.jpeg"
+  },
+  {
+    "revision": "5fe1f861098d8744bf118ac22260d80f",
+    "url": "/Workout-Tracker-fe-pt/static/media/diet13.5fe1f861.jpeg"
+  },
+  {
+    "revision": "999848c4de4d4dc0a1789c239c10d1da",
+    "url": "/Workout-Tracker-fe-pt/static/media/diet14.999848c4.jpeg"
+  },
+  {
+    "revision": "1843441918c9e1bb2e04f2a2c977d79b",
+    "url": "/Workout-Tracker-fe-pt/static/media/diet15.18434419.jpeg"
+  },
+  {
+    "revision": "acfba6678bbd062fe68269a79ef974a1",
+    "url": "/Workout-Tracker-fe-pt/static/media/diet2.acfba667.jpeg"
+  },
+  {
+    "revision": "54962c0c1d3ef37b07ef246e43e1e98e",
+    "url": "/Workout-Tracker-fe-pt/static/media/diet3.54962c0c.jpeg"
+  },
+  {
+    "revision": "fbfe1da86c106a7b4d8df72344926b09",
+    "url": "/Workout-Tracker-fe-pt/static/media/diet4.fbfe1da8.jpeg"
+  },
+  {
+    "revision": "367357e5af27099ee571997faf0cdf26",
+    "url": "/Workout-Tracker-fe-pt/static/media/diet5.367357e5.jpeg"
+  },
+  {
+    "revision": "743c23652d2c2c2451d5e49bffc3d7d6",
+    "url": "/Workout-Tracker-fe-pt/static/media/diet6.743c2365.jpeg"
+  },
+  {
+    "revision": "ffbae5941ad8149a7bdb9d3134d4672c",
+    "url": "/Workout-Tracker-fe-pt/static/media/diet7.ffbae594.jpeg"
+  },
+  {
+    "revision": "9763be4775a0ede264827ff1b2d60c68",
+    "url": "/Workout-Tracker-fe-pt/static/media/diet8.9763be47.jpeg"
+  },
+  {
+    "revision": "5f2a41dabdf11ac999c2a438da4a5d8b",
+    "url": "/Workout-Tracker-fe-pt/static/media/diet9.5f2a41da.jpeg"
+  },
+  {
+    "revision": "20a5cecccf2ec1964076f912d08be097",
+    "url": "/Workout-Tracker-fe-pt/static/media/food.20a5cecc.svg"
+  },
+  {
+    "revision": "5dde746739bb33a99eaa862597b207e4",
+    "url": "/Workout-Tracker-fe-pt/static/media/guycooking.5dde7467.svg"
+  },
+  {
+    "revision": "eaa9747d5135e18e2bd5fc96f4870096",
+    "url": "/Workout-Tracker-fe-pt/static/media/house.eaa9747d.svg"
+  },
+  {
+    "revision": "686737aa0dfcfe1331571f0b8cc62572",
+    "url": "/Workout-Tracker-fe-pt/static/media/icon.686737aa.svg"
+  },
+  {
+    "revision": "389d7869dfc5e74c368df3ac5bc38c40",
+    "url": "/Workout-Tracker-fe-pt/static/media/image1.389d7869.svg"
+  },
+  {
+    "revision": "fa8a3fa0bdcdc1cbd2b69a2d5c9524c6",
+    "url": "/Workout-Tracker-fe-pt/static/media/leftarrow.fa8a3fa0.svg"
+  },
+  {
+    "revision": "74eaefca29a06e31560057177cb1a130",
+    "url": "/Workout-Tracker-fe-pt/static/media/password.74eaefca.svg"
+  },
+  {
+    "revision": "29186e83bf763606e2ce50933ccc7cc7",
+    "url": "/Workout-Tracker-fe-pt/static/media/profilepic.29186e83.svg"
+  },
+  {
+    "revision": "1af66f67c3c408df18ad358c9d7dc242",
+    "url": "/Workout-Tracker-fe-pt/static/media/rightarrow.1af66f67.svg"
+  },
+  {
+    "revision": "e7f3bf339247ff901d455fd53bf6c039",
+    "url": "/Workout-Tracker-fe-pt/static/media/schedule.e7f3bf33.svg"
+  },
+  {
+    "revision": "40cad1c26eff902a7db7255b2b9cf34f",
+    "url": "/Workout-Tracker-fe-pt/static/media/searchicon.40cad1c2.svg"
+  },
+  {
+    "revision": "8cbe7937148702fe7b8e9b2480c18bdf",
+    "url": "/Workout-Tracker-fe-pt/static/media/smfullsircle.8cbe7937.svg"
+  },
+  {
+    "revision": "425d2d40c333debfb8b508b44c46f676",
+    "url": "/Workout-Tracker-fe-pt/static/media/weboncomputer.425d2d40.svg"
+  },
+  {
+    "revision": "f8999bfe069f7f18ce68ce8f7f9eeac6",
+    "url": "/Workout-Tracker-fe-pt/static/media/womanexercise.f8999bfe.svg"
+  },
+  {
+    "revision": "a27202ed87f7ffc805436f4059945628",
+    "url": "/Workout-Tracker-fe-pt/static/media/workout.a27202ed.svg"
+  },
+  {
+    "revision": "198061e8264c2e855d97eeaae53c01be",
+    "url": "/Workout-Tracker-fe-pt/static/media/workout1.198061e8.jpeg"
+  },
+  {
+    "revision": "8ef76168fe325e7fc1047418c28c78f9",
+    "url": "/Workout-Tracker-fe-pt/static/media/workout11.8ef76168.jpeg"
+  },
+  {
+    "revision": "2ff8a683ff923a8f8874f88e2f0a8910",
+    "url": "/Workout-Tracker-fe-pt/static/media/workout12.2ff8a683.jpg"
+  },
+  {
+    "revision": "0da7174555b7e9451d88914665c99d87",
+    "url": "/Workout-Tracker-fe-pt/static/media/workout14.0da71745.jpeg"
+  },
+  {
+    "revision": "7c69027dccaff643cdc921c45a336cec",
+    "url": "/Workout-Tracker-fe-pt/static/media/workout2.7c69027d.jpeg"
+  },
+  {
+    "revision": "fa7d9ba68a8b9761e509d5cd69f37553",
+    "url": "/Workout-Tracker-fe-pt/static/media/workout3.fa7d9ba6.jpeg"
+  },
+  {
+    "revision": "8b7590077685f290fbac9cd6dc180474",
+    "url": "/Workout-Tracker-fe-pt/static/media/workout4.8b759007.jpeg"
+  },
+  {
+    "revision": "6bf2444128339d1282d6391655cb121f",
+    "url": "/Workout-Tracker-fe-pt/static/media/workout5.6bf24441.jpeg"
+  },
+  {
+    "revision": "a2188e16806c0d97a85a765c074c0021",
+    "url": "/Workout-Tracker-fe-pt/static/media/workout6.a2188e16.jpeg"
+  },
+  {
+    "revision": "f4b9c047f688b4d0706f132252d15b68",
+    "url": "/Workout-Tracker-fe-pt/static/media/workout7.f4b9c047.jpeg"
+  },
+  {
+    "revision": "a8720cf0480da125231392851e349ba4",
+    "url": "/Workout-Tracker-fe-pt/static/media/workout8.a8720cf0.jpeg"
+  },
+  {
+    "revision": "ceb8acd21d7b0816039d350147409fdf",
+    "url": "/Workout-Tracker-fe-pt/static/media/workout9.ceb8acd2.jpeg"
+  },
+  {
+    "revision": "f77bf8ba52b58ae83c13c78855ffe36e",
+    "url": "/Workout-Tracker-fe-pt/static/media/workouttrainer.f77bf8ba.svg"
+  }
+]);
